b1dabdfcfd1936da230d5316b3674c163e02ad0c
alxgross <49441343+alxgross@users.noreply.github.com>
2020-10-27 10:26:22 +0100

    Update loxone_websockets_demo.py

--- loxone_websockets_demo.py
+++ loxone_websockets_demo.py
@@ -20,25 +20,27 @@
 # 5) wait for something to happen (maybe you now press some key in your home...)
 
 #Imports
-#Install pyCryptoDome
-import requests
-import websockets
-import asyncio
+import requests   #lib for GET, POST, PUT etc.
+import websockets #lib for websockets
+import asyncio    #Asynchronous operation is necessary to deal with websockets
+
+#Install pyCryptoDome NOT pyCrypto
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import AES
 from Crypto import Random
 from Crypto.Cipher import PKCS1_v1_5
-import base64
-import secrets
-import binascii
-import json
-import hashlib
-import hmac
-import urllib
+
+import base64    #necessary to encode in Base64
+import secrets   #helpful to produce hashes and random bytes
+import binascii  #hexlify/unhexlify allows to get HEX-Strings out of bytes-variables
+import json      #working with JSON
+import hashlib   #Hashing
+import hmac      #Key-Hashing
+import urllib    #necessary to encode URI-compliant
 
 #Some Configuration/Definition --> Edit as needed
 
-#Fixed values (for demo purpose only) - should be replaced by randomly generated
+#Fixed values (for demo purpose only) - should be replaced by randomly generated (page 7, step 4 & 5)
 aes_key = str("6A586E3272357538782F413F4428472B4B6250655368566B5970337336763979")
 aes_iv = str("782F413F442A472D4B6150645367566B")
 
@@ -52,46 +54,50 @@ myUUID = "093302e1-02b4-603c-ffa4ege000d80cfd" #A UUID of your choosing --> you 
 myIdentifier = "lox_test_script" #an identifier of your chosing
 myPermission = 2 #2 for short period, 4 for long period
 
-rsa_pub_key = None #possibility to set the key for debugging "-----BEGIN PUBLIC KEY-----\nMxxxvddfDCBiQKBgQCvuJAG7r0FdysdfsdfBl/dDbxyu1h0KQdsf7cmm7mhnNPCevRVjRB+nlK5lljt1yMqJtoQszZqCuqP8ZKKOL1gsp7F0E+xgZjOpsNRcLxglGImS6ii0oTiyDgAlS78+mZrYwvow3d05eQlhz6PzqhAh9ZHQIDAQAB\n-----END PUBLIC KEY-----"
+rsa_pub_key = None #possibility to set the key for debugging, e.g. "-----BEGIN PUBLIC KEY-----\nMxxxvddfDCBiQKBgQCvuJAG7r0FdysdfsdfBl/dDbxyu1h0KQdsf7cmm7mhnNPCevRVjRB+nlK5lljt1yMqJtoQszZqCuqP8ZKKOL1gsp7F0E+xgZjOpsNRcLxglGImS6ii0oTiyDgAlS78+mZrYwvow3d05eQlhz6PzqhAh9ZHQIDAQAB\n-----END PUBLIC KEY-----"
+
+### THIS is the actual program that executes - type: python loxone_websockets_demo.py ###
+
+rsa_pub_key = prepareRsaKey() #Retrieve the public RSA key of the miniserver (page 7, step 2)
+asyncio.get_event_loop().run_until_complete(webSocketLx()) #Start the eventloop (async) with the function webSocketLx
 
 
-
-#https://www.base64encode.org/
-#urlsafe_b64 = "SG5qRzRIbk8rRDc5dDNhdEUxdi9BYlB6aDMxcjBiTUhLZnVDVSsySmdVT0xMRUg0OURvajg5d2dUU0FySm1uMzJTYkR6UkkrZXBRa3FZc2tCbGtnR3J1RW9WYVZhZWQxRHBYNi9vRGg0QWtUUDUzNWpad2dvMWc4WUMwUEZjSW01R3V4TlJ5ZDFRcWNtdnBNNzIvZ3E4SkJqRVpGZXh4WWwybnJpVE5EdUVNPQ"
-
+### These are the functions used ###
 
 #Async Functions
 
 #Websocket connection to Loxone
 async def webSocketLx():
-    #start connection
+    
+    #Encrypt the AES Key and IV with RSA (page 7, step 6)
+    sessionkey = await create_sessionkey(aes_key, aes_iv)
+    print("Session key: ", sessionkey) 
+    
+    #start websocket connection (page 7, step 3 - protocol does not need to be specified apparently)
     async with websockets.connect("ws://{}:{}/ws/rfc6455".format(myIP, myPort)) as myWs:
         
-        #Encrypt the AES Key and IV with RSA
-        sessionkey = await create_sessionkey(aes_key, aes_iv)
-        print(sessionkey)
-
-        #Send Session Key
+        #Send Session Key (page 8, step 7)
         await myWs.send("jdev/sys/keyexchange/{}".format(sessionkey))
         await myWs.recv()
         response = await myWs.recv()
         sessionkey_answer = json.loads(response)["LL"]["value"]
         
-        #Now a ramdom salt of 2 bytes
+        #Now a ramdom salt of 2 bytes is added (page 8, step 8)
         aes_salt = binascii.hexlify(secrets.token_bytes(2)).decode()
-        print(aes_salt)
         
         #Now prepare the token collection command with command encryption
         #Objective is to: Request a JSON Web Token “jdev/sys/getjwt/{hash}/{user}/{permission}/{uuid}/{info}”
-        #○ This request must be encrypted
+        #--> This request must be encrypted
+        # page 8, step 9b
         
         #Sending encrypted commands over the websocket (page 27, step 1)
+        # Get the JSON web token (page 22, 23)
         getTokenCommand = "salt/{}/jdev/sys/getjwt/{}/{}/{}/{}/{}".format(aes_salt, await hashUserPw(myUser, myPassword), myUser, myPermission, myUUID, myIdentifier)
         print(getTokenCommand)
         
-        #Now encrypt the command with AES
+        #Now encrypt the command with AES (page 21 step 1 & 2)
         encrypted_command = await aes_enc(getTokenCommand, aes_key, aes_iv)
-        message_to_ws = "jdev/sys/enc/{}".format(encrypted_command)
+        message_to_ws = "jdev/sys/enc/{}".format(encrypted_command) # page 21, step 3
         print("Message to be sent: ", message_to_ws)
         
         #Send message to get a JSON webtoken
@@ -99,7 +105,8 @@ async def webSocketLx():
         await myWs.recv()
         print(await myWs.recv()) #And if you get back a 200 the connection is established
         
-
+        
+# Function to RSA encrypt the AES key and iv
 async def create_sessionkey(aes_key, aes_iv):
     payload = aes_key + ":" + aes_iv
     payload_bytes = payload.encode()
@@ -109,9 +116,11 @@ async def create_sessionkey(aes_key, aes_iv):
     pub_key = RSA.importKey(rsa_pub_key)
     encryptor = PKCS1_v1_5.new(pub_key)
     sessionkey = encryptor.encrypt(payload_bytes)
+    #https://www.base64encode.org/ to compare
     return base64.standard_b64encode(sessionkey).decode()
     
     
+# AES encrypt with the shared AES Key and IV    
 async def aes_enc(text, aes_key, aes_iv):
     key = binascii.unhexlify(aes_key)
     iv = binascii.unhexlify(aes_iv)
@@ -120,11 +129,16 @@ async def aes_enc(text, aes_key, aes_iv):
     encrypted_msg = encoder.encrypt(await pad(text.encode()))
     b64encoded = base64.standard_b64encode(encrypted_msg)
     return urllib.parse.quote(b64encoded, safe="") #Return url-Encrypted
-    
+ 
+
+# ZeroBytePadding to AES block size (16 byte) to allow encryption 
 async def pad(byte_msg):
     return byte_msg + b"\0" * (AES.block_size - len(byte_msg) % AES.block_size) #ZeroBytePadding / Zero Padding
 
+
+# Key-Hash the User and Password HMAC-SHA1 (page 22)
 async def hashUserPw(user, password):
+    # Get the key to be used for the HMAC-Hashing and the Salt to be used for the SHA1 hashing
     response = requests.get("http://{}:{}/jdev/sys/getkey2/{}".format(myIP, myPort, user))
     print(response.text)
     userKey = response.json()["LL"]["value"]["key"]
@@ -135,6 +149,8 @@ async def hashUserPw(user, password):
     #The userHash shall be left like it is
     return userHash
     
+
+# Hash the Password plain and simple: SHA1 (page 22)
 async def hash_Password(password, userSalt):
     #check if result is this: https://passwordsgenerator.net/sha1-hash-generator/
     tobehashed = password + ":" + userSalt
@@ -145,6 +161,8 @@ async def hash_Password(password, userSalt):
     print("Hashed: ", hashstring.upper())
     return hashstring.upper()
     
+
+# HMAC-SHA1 hash something with a given key
 async def digest_hmac_sha1(message, key):
     #https://gist.github.com/heskyji/5167567b64cb92a910a3
     #compare: https://www.liavaag.org/English/SHA-Generator/HMAC/  -- key type: text, output: hex
@@ -161,8 +179,9 @@ async def digest_hmac_sha1(message, key):
     #return a hex string
     return signature2.decode()
     
-#sync part
-        
+    
+### sync functions ###
+# Get the RSA public key from the miniserver and format it so that it is compliant with a .PEM file 
 def prepareRsaKey():
     response = requests.get("http://{}:{}/jdev/sys/getPublicKey".format(myIP, myPort))
     rsa_key_malformed = response.json()["LL"]["value"]
@@ -171,5 +190,4 @@ def prepareRsaKey():
     print("RSA Public Key: ", rsa_key_wellformed)
     return rsa_key_wellformed
     
-rsa_pub_key = prepareRsaKey()
-asyncio.get_event_loop().run_until_complete(webSocketLx())
+
